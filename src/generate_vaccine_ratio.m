e4e1d54421dad7162699d3e785b9395bf7533193
이윤정 <yunjung0104@gmail.com>
2021-09-07 12:48:36 +0900

    change the number of rows having 0's

--- src/generate_vaccine_ratio.m
+++ src/generate_vaccine_ratio.m
@@ -168,7 +168,7 @@ varnames = {'0-9', '10-19', '20-29', '30-39', '40-49', '50-59', '60-69', '70-79'
 
 % Add zeros in front of 2021/02/25
 vaccine_1st = [zeros(10, size(vaccine_1st, 2)); vaccine_1st];
-vaccine_2nd = [zeros(10, size(vaccine_2nd, 2)); vaccine_2nd];
+vaccine_2nd = [zeros(11, size(vaccine_2nd, 2)); vaccine_2nd];
 
 % Change into table
 vaccine_1st = array2table(vaccine_1st, 'RowNames', rownames, 'VariableNames', varnames);
