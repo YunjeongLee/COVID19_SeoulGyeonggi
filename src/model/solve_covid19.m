87f7a9ac8576d27c54d5c2c9582372148b3cb4eb
YunjeongLee <yunjung0104@gmail.com>
2021-09-08 14:42:17 +0900

    `FOI_V` -> `FOI`

--- src/model/solve_covid19.m
+++ src/model/solve_covid19.m
@@ -65,7 +65,7 @@ for i = 1:length(tspan_)
         I(in, :) = I(ic, :) + dt_ * (kappa_ .* E(ic, :) - alpha_ .* I(ic, :));
         H(in, :) = H(ic, :) + dt_ * (alpha_ .* I(ic, :) - gamma_ .* H(ic, :));
         R(in, :) = R(ic, :) + dt_ * (gamma_ .* H(ic, :) + num_dose2 .* vac_eff_t(2));
-        V(in, :) = V(ic, :) + dt_ * (num_dose1 .* vac_eff_t(2) - vac_1st_fail * FOI_V .* V(ic, :) - num_dose2 .* vac_eff_t(2));
+        V(in, :) = V(ic, :) + dt_ * (num_dose1 .* vac_eff_t(2) - vac_1st_fail * FOI .* V(ic, :) - num_dose2 .* vac_eff_t(2));
     end
 end
 
