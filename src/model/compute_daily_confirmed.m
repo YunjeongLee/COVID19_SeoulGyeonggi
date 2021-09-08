374ccde45154ac23b4fed98bf5dca0b818efd5dc
YunjeongLee <yunjung0104@gmail.com>
2021-09-08 11:05:36 +0900

    fix typo

--- src/model/compute_daily_confirmed.m
+++ src/model/compute_daily_confirmed.m
@@ -9,7 +9,7 @@ I = sol(:, 2*num_grp+1:3*num_grp);
 dt_confirmed = alpha_ .* (I(1:end-1, :) + I(2:end, :))/2 * dt_;
 
 num_days = length(dt_confirmed) * dt;
-multiplier4year = kron(eye(num_days), ones(1, 1/dt));
+multiplier4year = kron(eye(num_days), ones(1, 1/dt_));
 
 val = multiplier4year * dt_confirmed;
 
