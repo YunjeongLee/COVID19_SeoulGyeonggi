e7231ec22840849173deaca22835e1db47ff453d
YunjeongLee <yunjung0104@gmail.com>
2021-09-08 10:34:18 +0900

    delete unused functions

--- src/model/vaccine_efficacy.m
+++
@@ -1,7 +0,0 @@
-function val = vaccine_efficacy(t, vacc_eff)
-val = zeros(1, 2);
-tspan = [0:length(vacc_eff)-1, Inf];
-for i = 1:length(vacc_eff)
-    val = val + ((t >= tspan(i)) & (t < tspan(i+1))) * vacc_eff(i, :);
-end
-end
