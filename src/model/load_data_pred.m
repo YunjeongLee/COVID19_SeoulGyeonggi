683e952571063550c2def644f14c9fe93bcfa64e
YunjeongLee <yunjung0104@gmail.com>
2021-09-13 15:44:24 +0900

    edit range to load delta proportion data and vaccination data

--- src/model/load_data_pred.m
+++ src/model/load_data_pred.m
@@ -1,7 +1,7 @@
 function [data, date, delta_prop, contact, init_beta, y0, vaccine_1st, vaccine_2nd, vaccine_eff, cfr, severe] = load_data_pred()
 %% Load alpha and delta proportion
 filename = '../data/covid19/alpha_delta_effect.xlsx';
-delta_prop = readmatrix(filename, 'range', 'C2:C200');
+delta_prop = readmatrix(filename, 'range', 'C2:C321');
 
 %% Load contact matrix
 filename = '../data/contact/[2020서울경기]contact_rate_matrix.csv';
@@ -20,9 +20,9 @@ vaccine_number = vaccine_number{:, 2:end};
 
 %% Load vaccination ratio by age
 filename = '../data/vaccine/1st_dose_ratio_by_age.csv';
-vaccine_1st = readmatrix(filename, 'range', 'B2:J199');
+vaccine_1st = readmatrix(filename, 'range', 'B2:J321');
 filename = '../data/vaccine/2nd_dose_ratio_by_age.csv';
-vaccine_2nd = readmatrix(filename, 'range', 'B2:J199');
+vaccine_2nd = readmatrix(filename, 'range', 'B2:J321');
 
 %% Compute the vaccination number by age
 vaccine_1st = vaccine_number(:, 1) .* vaccine_1st;
