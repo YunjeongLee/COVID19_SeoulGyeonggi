7af764b58a9c89e9b43e7ed433df5bf14027c99c
YunjeongLee <yunjung0104@gmail.com>
2021-09-13 10:47:53 +0900

    select `sd_1st` as effect from 2021/07/01 to 2021/07/11

--- src/model/social_distance.m
+++ src/model/social_distance.m
@@ -2,7 +2,7 @@ function val = social_distance(t, sd_1st, sd_2nd)
 if t < 136
     val = 1;
 elseif (t >= 136) && (t < 147)
-    val = 1.685;
+    val = sd_1st;
 elseif (t >= 147)
     val = 1.685 * 0.67 * 0.33;
 end
