1ca27f1b090f618cc93f104ff97bb9dea7ce1fb4
YunjeongLee <yunjung0104@gmail.com>
2021-09-13 15:21:59 +0900

    edit filename

--- src/generate_vaccine_ratio.m
+++ src/generate_vaccine_ratio.m
@@ -77,7 +77,7 @@ for i = 1:5
 end
 
 %% 2021/05/02 to 2021/08/31 (1st dose)
-filename = "../data/vaccine/vaccination_12dose_05030830.xlsx";
+filename = "../data/vaccine/vaccination_12dose_05030913.xlsx";
 vaccine_after_0503 = readmatrix(filename, 'sheet', '1st dose', 'range', 'B2:H21');
 
 % Change into weekly incident vaccination
@@ -139,7 +139,7 @@ for i = 1:size(vaccine_before_0628, 1)
 end
 
 %% 2021/06/29 to 2021/08/31 (2nd dose)
-filename = "../data/vaccine/vaccination_12dose_05030830.xlsx";
+filename = "../data/vaccine/vaccination_12dose_05030913.xlsx";
 vaccine_after_0503 = readmatrix(filename, 'sheet', '2nd dose', 'range', 'B11:H21');
 
 % Change into weekly incident vaccination
