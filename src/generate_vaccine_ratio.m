24f7f93a18734b06af8e69b3c39d53572f97f27b
YunjeongLee <yunjung0104@gmail.com>
2021-09-13 15:34:41 +0900

    edit number of repeat

--- src/generate_vaccine_ratio.m
+++ src/generate_vaccine_ratio.m
@@ -153,7 +153,7 @@ for i = 1:size(vaccine_after_0503, 1)
     if i == 1
         num_to_repeat = 14;
     elseif i == size(vaccine_after_0503, 1)
-        num_to_repeat = 116;
+        num_to_repeat = 35;
     else
         num_to_repeat = 7;
     end
