5b101ac3c1db9c3aca934e692f41b2aa473d7464
YunjeongLee <yunjung0104@gmail.com>
2021-09-13 18:01:48 +0900

    change results folder name

--- src/main_prediction.m
+++ src/main_prediction.m
@@ -58,7 +58,7 @@ for i = 1:length(sd_3rd_val)
                   'school', school(j), false, 'School effect'};
         
         %% Visualize prediction
-        results_path = sprintf('../results/predict_sd_%d_school_%d', i, j);
+        results_path = sprintf('../results/predict_%d', (i-1)*length(school)+j);
         mkdir(results_path)
         visualize_pred(data, params, theta_mle, date, cfr, severe, results_path);
     end
