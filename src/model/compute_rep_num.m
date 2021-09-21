6a1a06c3cae42ae6cbd38c5e0e2b5d2b0d38d7ae
이윤정 <yunjung0104@gmail.com>
2021-09-21 18:20:04 +0900

    delete line defining `V` at time t

--- src/model/compute_rep_num.m
+++ src/model/compute_rep_num.m
@@ -39,7 +39,6 @@ for i = 1:length(tspan_)
         ic = (i-1)/dt_ + j;
         % S and V at time t
         St = S(ic, :);
-        Vt = V(ic, :);
         % School effect
         contact_temp = contact_;
         contact_temp(2, 2) = contact_(2, 2) .* school_effect(t, school_);
