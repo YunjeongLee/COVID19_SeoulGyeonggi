9d8767969c2246d843080ba775d48346f5dc3c45
이윤정 <yunjung0104@gmail.com>
2021-09-21 19:43:05 +0900

    add 3rd social distancing effect to `params`

--- src/main_estimate_mle_from_June.m
+++ src/main_estimate_mle_from_June.m
@@ -49,7 +49,8 @@ for i = 1:length(sd_1st_val)
             'hosp_2nd', 1-0.94, false, 'hospitalization risk after 2nd dose';
             % Social distancing effect
             'sd_1st', sd_1st_val(i), false, '1st social distancing effect';
-            'sd_2nd', sd_1st_val(i) * sd_2nd_val(j), false, '2nd social distancing effect'};
+            'sd_2nd', sd_1st_val(i) * sd_2nd_val(j), false, '2nd social distancing effect';
+            'sd_3rd', sd_1st_val(i) * sd_2nd_val(j), false, '3rd social distancing effect';
         
         %% Parameter estimation (2021/02/15 ~ 2021/05/31)
         isEstimated = cell2mat(params(:, 3));
