fe2e69f321b71c9153a7e10357c4316ff5ecb700
이윤정 <yunjung0104@gmail.com>
2021-09-21 18:17:07 +0900

    edit difference equation for `H`

--- src/model/solve_covid19.m
+++ src/model/solve_covid19.m
@@ -79,7 +79,7 @@ for i = 1:length(tspan_)
         I(in, :) = I(ic, :) + dt_ * (kappa_ .* E(ic, :) - alpha_ .* I(ic, :));
         Iv1(in, :) = Iv1(ic, :) + dt_ * (kappa_ .* Ev1(ic, :) - alpha_ .* Iv1(ic, :));
         Iv2(in, :) = Iv2(ic, :) + dt_ * (kappa_ .* Ev2(ic, :) - alpha_ .* Iv2(ic, :));
-        H(in, :) = H(ic, :) + dt_ * (alpha_ .* I(ic, :) - gamma_ .* H(ic, :));
+        H(in, :) = H(ic, :) + dt_ * (alpha_ .* (I(ic, :) + Iv1(ic, :) + Iv2(ic, :)) - gamma_ .* H(ic, :));
         R(in, :) = R(ic, :) + dt_ * (gamma_ .* H(ic, :) + num_dose2 .* vac_eff_t(2));
     end
 end
