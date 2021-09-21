aed8bc4c61a0ec951e8a2c5cd28e52859c63e37b
이윤정 <yunjung0104@gmail.com>
2021-09-21 18:35:07 +0900

    change variable name `dt_confirmed` -> `dt_death`

--- src/model/compute_daily_death.m
+++ src/model/compute_daily_death.m
@@ -13,9 +13,9 @@ dt_confirmed = alpha_ .* (I(1:end-1, :) + I(2:end, :))/2 * dt_ ...
     + alpha_ .* (Iv1(1:end-1, :) + Iv1(2:end, :))/2 * dt_ ...
     + alpha_ .* (Iv2(1:end-1, :) + Iv2(2:end, :))/2 * dt_;
 
-num_days = length(dt_confirmed) * dt_;
+num_days = length(dt_death) * dt_;
 multiplier4year = kron(eye(num_days), ones(1, 1/dt_));
 
-val = multiplier4year * dt_confirmed;
+val = multiplier4year * dt_death;
 
 end
