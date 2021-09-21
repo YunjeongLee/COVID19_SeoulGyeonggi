16f0be5271e0b100ca5c6995b5435819becd543f
이윤정 <yunjung0104@gmail.com>
2021-09-21 18:41:22 +0900

    change variable name `dt_confirmed` -> `dt_severe`

--- src/model/compute_daily_hospitalized.m
+++ src/model/compute_daily_hospitalized.m
@@ -15,9 +15,9 @@ dt_confirmed_I = alpha_ .* (I(1:end-1, :) + I(2:end, :))/2 * dt_;
 dt_confirmed_Iv1 = alpha_ .* (Iv1(1:end-1, :) + Iv1(2:end, :))/2 * dt_;
 dt_confirmed_Iv2 = alpha_ .* (Iv2(1:end-1, :) + Iv2(2:end, :))/2 * dt_;
 
-num_days = length(dt_confirmed) * dt_;
+num_days = length(dt_severe) * dt_;
 multiplier4year = kron(eye(num_days), ones(1, 1/dt_));
 
-val = multiplier4year * dt_confirmed;
+val = multiplier4year * dt_severe;
 
 end
