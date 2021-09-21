92c16adf54bee5306df1ca6d4b0e09c079e41626
이윤정 <yunjung0104@gmail.com>
2021-09-21 18:32:24 +0900

    extract `Iv2` from `sol

--- src/model/compute_daily_confirmed.m
+++ src/model/compute_daily_confirmed.m
@@ -5,6 +5,7 @@ dt_ = parameter.dt;
 num_grp = size(parameter.contact, 1);
 I = sol(:, 6*num_grp+1:7*num_grp);
 Iv1 = sol(:, 7*num_grp+1:8*num_grp);
+Iv2 = sol(:, 8*num_grp+1:9*num_grp);
 
 %% Compute daily confirmed cases
 dt_confirmed = alpha_ .* (I(1:end-1, :) + I(2:end, :))/2 * dt_;
