5bc7474c45686fd13ae9fbcfaa87da1fd38e7a97
이윤정 <yunjung0104@gmail.com>
2021-09-21 18:40:59 +0900

    split incident confirmed cases into `I`, `Iv1` and `Iv2`

--- src/model/compute_daily_hospitalized.m
+++ src/model/compute_daily_hospitalized.m
@@ -11,9 +11,9 @@ Iv1 = sol(:, 7*num_grp+1:8*num_grp);
 Iv2 = sol(:, 8*num_grp+1:9*num_grp);
 
 %% Compute daily confirmed cases
-dt_confirmed = alpha_ .* (I(1:end-1, :) + I(2:end, :))/2 * dt_ ...
-    + alpha_ .* (Iv1(1:end-1, :) + Iv1(2:end, :))/2 * dt_ ...
-    + alpha_ .* (Iv2(1:end-1, :) + Iv2(2:end, :))/2 * dt_;
+dt_confirmed_I = alpha_ .* (I(1:end-1, :) + I(2:end, :))/2 * dt_;
+dt_confirmed_Iv1 = alpha_ .* (Iv1(1:end-1, :) + Iv1(2:end, :))/2 * dt_;
+dt_confirmed_Iv2 = alpha_ .* (Iv2(1:end-1, :) + Iv2(2:end, :))/2 * dt_;
 
 num_days = length(dt_confirmed) * dt_;
 multiplier4year = kron(eye(num_days), ones(1, 1/dt_));
