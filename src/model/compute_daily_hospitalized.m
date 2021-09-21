5b1b8b4cd8f330c0f24a00c27d605f7df68e87b2
이윤정 <yunjung0104@gmail.com>
2021-09-21 18:41:31 +0900

    compute severe cases

--- src/model/compute_daily_hospitalized.m
+++ src/model/compute_daily_hospitalized.m
@@ -15,6 +15,13 @@ dt_confirmed_I = alpha_ .* (I(1:end-1, :) + I(2:end, :))/2 * dt_;
 dt_confirmed_Iv1 = alpha_ .* (Iv1(1:end-1, :) + Iv1(2:end, :))/2 * dt_;
 dt_confirmed_Iv2 = alpha_ .* (Iv2(1:end-1, :) + Iv2(2:end, :))/2 * dt_;
 
+% Compute severe cases
+dt_severe_I = dt_confirmed_I .* severe_;
+dt_severe_Iv1 = dt_confirmed_Iv1 .* severe_ .* hosp_1st_;
+dt_severe_Iv2 = dt_confirmed_Iv2 .* severe_ .* hosp_2nd_;
+
+dt_severe = dt_severe_I + dt_severe_Iv1 + dt_severe_Iv2;
+
 num_days = length(dt_severe) * dt_;
 multiplier4year = kron(eye(num_days), ones(1, 1/dt_));
 
