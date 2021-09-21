6f3ff0998638e49f054d4d7865cffba2a6e4eb96
이윤정 <yunjung0104@gmail.com>
2021-09-21 16:55:47 +0900

    add `cfr` and `severe` to `params`

--- src/main_estimate_mle_before_June.m
+++ src/main_estimate_mle_before_June.m
@@ -41,7 +41,10 @@ params = {% Parameters to be estimated
           'vac_eff', vaccine_eff, false, 'vaccine efficacy';
           % Social distancing effect
           'sd_1st', sd_1st, false, '1st social distancing effect';
-          'sd_2nd', sd_2nd, false, '2nd social distancing effect'};
+          'sd_2nd', sd_2nd, false, '2nd social distancing effect';
+          % CFR or severeness
+          'cfr', cfr, false, 'case fatality rate';
+          'severe', severe, false, 'severity'};
 
 %% Parameter estimation (2021/02/15 ~ 2021/05/31)
 isEstimated = cell2mat(params(:, 3));
