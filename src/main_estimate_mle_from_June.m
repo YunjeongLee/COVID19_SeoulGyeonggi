641ddb0b6f0a4a59562cd2b596ad888edee6495b
이윤정 <yunjung0104@gmail.com>
2021-09-21 19:46:18 +0900

    edit index of 1st social distancing effect

--- src/main_estimate_mle_from_June.m
+++ src/main_estimate_mle_from_June.m
@@ -52,7 +52,7 @@ school = 1;
             % Social distancing effect
             'sd_1st', sd_1st_val(i), false, '1st social distancing effect';
             'sd_2nd', sd_1st_val(i) * sd_2nd_val(j), false, '2nd social distancing effect';
-            'sd_3rd', sd_1st_val(i) * sd_2nd_val(j), false, '3rd social distancing effect';
+            'sd_3rd', sd_1st_val * sd_2nd_val(j), false, '3rd social distancing effect';
             % School effect
             'school', school, false, 'School effect';
             % CFR or severeness
