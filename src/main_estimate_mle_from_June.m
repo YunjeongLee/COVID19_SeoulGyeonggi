2592085e6ff1684f211ab160888fde903cfabdb6
이윤정 <yunjung0104@gmail.com>
2021-09-23 13:39:57 +0900

    change the order of second social distancing effect

--- src/main_estimate_mle_from_June.m
+++ src/main_estimate_mle_from_June.m
@@ -22,7 +22,7 @@ tspan = 0:size(data, 1)-1;
 
 % Social distancing effect
 sd_1st_val = 1 + 0.8322/2;
-sd_2nd_val = [0.699 * 0.35, 0.699, 0.35, (0.699+0.35)/2];
+sd_2nd_val = [0.699 * 0.35, 0.35, (0.699+0.35)/2, 0.699];
 sd_3rd_val = sd_2nd_val;
 
 % School effect
