7c8fba3e8c1c16f3572fa0a77293836f7779f017
이윤정 <yunjung0104@gmail.com>
2021-09-24 14:41:44 +0900

    delete `cfr` and `severe` from inputs of `visualize_pred`

--- src/main_prediction.m
+++ src/main_prediction.m
@@ -67,7 +67,7 @@ for i = 1:length(sd_3rd_val)
         %% Visualize prediction
         results_path = sprintf('../results/predict_exp_%d_%d', k, (i-1)*length(school)+j);
         mkdir(results_path)
-        visualize_pred(data, params, theta_mle, date, cfr, severe, results_path);
+        visualize_pred(data, params, theta_mle, date, results_path);
     end
 end
 end
