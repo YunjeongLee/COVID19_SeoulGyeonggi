ceda5deb2a87603ce03d4a25feaa7f4b4db0444a
YunjeongLee <yunjung0104@gmail.com>
2021-10-22 17:12:17 +0900

    define `dt_death_Iv2`

--- src/model/compute_daily_death.m
+++ src/model/compute_daily_death.m
@@ -17,6 +17,7 @@ dt_confirmed_Iv2 = alpha_ .* (Iv2(1:end-1, :) + Iv2(2:end, :))/2 * dt_;
 
 dt_death_I = dt_confirmed_I .* cfr_;
 dt_death_Iv1 = dt_confirmed_Iv1 .* cfr_ .* death_1st_;
+dt_death_Iv2 = dt_confirmed_Iv2 .* cfr_ .* death_2nd_;
 num_days = length(dt_death) * dt_;
 multiplier4year = kron(eye(num_days), ones(1, 1/dt_));
 
