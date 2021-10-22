0c8e08cc39b5e04e1937dcfa2275be180f93902a
YunjeongLee <yunjung0104@gmail.com>
2021-10-22 17:09:53 +0900

    change variablenames (`dt_death` -> `dt_death_I`, `dt_confirmed` -> `dt_confirmed_I`)

--- src/model/compute_daily_death.m
+++ src/model/compute_daily_death.m
@@ -13,7 +13,7 @@ dt_confirmed_I = alpha_ .* (I(1:end-1, :) + I(2:end, :))/2 * dt_;
 dt_confirmed_Iv1 = alpha_ .* (Iv1(1:end-1, :) + Iv1(2:end, :))/2 * dt_;
 dt_confirmed_Iv2 = alpha_ .* (Iv2(1:end-1, :) + Iv2(2:end, :))/2 * dt_;
 
-dt_death = dt_confirmed .* cfr_;
+dt_death_I = dt_confirmed_I .* cfr_;
 num_days = length(dt_death) * dt_;
 multiplier4year = kron(eye(num_days), ones(1, 1/dt_));
 
