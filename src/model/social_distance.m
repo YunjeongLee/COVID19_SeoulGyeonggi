4e870bc8a0f582e18353ff7cd34f3159fbd4f417
이윤정 <yunjung0104@gmail.com>
2021-10-26 22:27:19 +0900

    add input `sd_4th` to `social_distance` function

--- src/model/social_distance.m
+++ src/model/social_distance.m
@@ -1,4 +1,4 @@
-function val = social_distance(t, sd_1st, sd_2nd, sd_3rd)
+function val = social_distance(t, sd_1st, sd_2nd, sd_3rd, sd_4th)
 if t < 136
     val = 1;
 elseif (t >= 136) && (t < 147)
